399c9ae5bf99f375bb761c67e53b678681e81ffb
Nate Swift <nateswift@tests-MacBook-Pro.local>
2018-07-26 17:16:53 -0600

    Made header words change JS

--- dev/src/app/components/intro/intro.component.ts
+++ dev/src/app/components/intro/intro.component.ts
@@ -7,18 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroComponent implements OnInit {
 
-  adj = 'Creative';
-  adjs = ['Creative', 'Custom', 'Unique', 'Awesome'];
+  adj = '';
+  adjs = ['Creative', 'Custom', 'Unique', 'Expressive'];
   currentAdj = 0;
 
+  verb = '';
+  verbs = ['Design','Develop', 'Create', 'Produce'];
+  currentVerb = 0;
+
+  noun = '';
+  nouns = ['Webpages', 'Projects', 'Experiences', 'Art'];
+  currentNoun = 0;
+
+
   constructor() {
+    this.setWords();
     setInterval(() => {
       this.currentAdj++;
-      if (this.currentAdj === this.adjs.length) {
-        this.currentAdj = 0;
-      }
-      this.adj = this.adjs[this.currentAdj];
+      this.currentVerb++;
+      this.currentNoun++;
+      if (this.currentAdj === this.adjs.length) this.currentAdj = 0;
+      if (this.currentVerb === this.verbs.length) this.currentVerb = 0;
+      if (this.currentNoun === this.nouns.length) this.currentNoun = 0;
+      this.setWords();
     }, 2000);
+  }
+
+  setWords() {
+    this.verb = this.verbs[this.currentVerb];
+    this.adj = this.adjs[this.currentAdj];
+    this.noun = this.nouns[this.currentNoun];
   }
 
   ngOnInit() {
